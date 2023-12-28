6126bdca765507e6a7df30ca1056a7b420b922dc
Sabrynna <limasabrynna03@gmail.com>
2023-12-27 21:52:36 -0300

    secao 3 - exercicio 3 a 8

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -26,14 +26,49 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('input[id="lastName"]').should('be.visible').type('Lourenço').should('have.value', 'Lourenço')
         cy.get('input[type="email"]').should('be.visible').type('limasabrynna@gmail.com').should('have.value', 'limasabrynna@gmail.com')
         cy.get('textarea[id="open-text-area"]').should('be.visible').type('Cadastro de usuário exatamente digitado rápido', { delay: 0 }).should('have.value', 'Cadastro de usuário exatamente digitado rápido')
-        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.contains('button', 'Enviar').should('be.visible').click()
+
         cy.get('span[class="success"]').should('be.visible')
     })
 
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
         cy.get('input[type="email"]').should('be.visible').type('limasabrynnagmail.com')
-        cy.get('button[type="submit"]').should('be.visible').click()
+        cy.contains('button', 'Enviar').should('be.visible').click()
         cy.get('span[class="error"]').should('be.visible')
+    })
+
+    it('campo telefone continua vazio quando preenchido com valor não numérico', function() {
+        cy.get('#phone').type('abcdefghij').should('have.value', '')
+    })
+
+    it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('input[id="firstName"]').type('Sabrynna')
+        cy.get('input[id="lastName"]').type('Lourenço')
+        cy.get('input[type="email"]').type('limasabrynna@gmail.com')
+        cy.get('#phone-checkbox').click()
+        cy.get('textarea[id="open-text-area"]').type('Cadastro de usuário exatamente digitado rápido')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('preenche e limpa os campos nome, sobrenome, email e telefone', function() {
+        cy.get('#firstName').type('Sabrynna').should('have.value', 'Sabrynna').clear().should('have.value', '')
+        cy.get('#lastName').type('Lourenço').should('have.value', 'Lourenço').clear().should('have.value', '')
+        cy.get('#email').type('limasabrynna@gmail.com').should('have.value', 'limasabrynna@gmail.com').clear().should('have.value', '')
+        cy.get('#phone').type('83988895874').should('have.value', '83988895874').clear().should('have.value', '')
+    })
+
+    it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
+    })
+
+    it('envia o formuário com sucesso usando um comando customizado', function() {
+        cy.fillMandatoryFieldsAndSubmit()
+
+        cy.get('span[class="success"]').should('be.visible')
     })
 })
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,7 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('fillMandatoryFieldsAndSubmit', function() {
+    cy.get('input[id="firstName"]').type('Sabrynna')
+    cy.get('input[id="lastName"]').type('Lourenço')
+    cy.get('input[type="email"]').type('limasabrynna@gmail.com')
+    cy.get('textarea[id="open-text-area"]').type('Cadastro de usuário exatamente digitado rápido')
+    cy.contains('button', 'Enviar').click()
+})
