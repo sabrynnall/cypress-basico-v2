c20041bcaa09ed0e54b4a51a49e0cde47898ae6c
Sabrynna <limasabrynna03@gmail.com>
2023-12-28 21:06:56 -0300

    exercicios secao 6

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -87,11 +87,26 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('input[type="radio"][value="feedback"').check().should('have.value', 'feedback')
     })
 
-    it.only('marca cada tipo de atendimento', function() {
+    it('marca cada tipo de atendimento', function() {
         cy.get('input[type="radio"]').should('have.length', 3).each(function($radio) {
             cy.wrap($radio).check()
             cy.wrap($radio).should('be.checked')
         })
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+        cy.get('input[type="checkbox"]').check().should('be.checked').last().uncheck().should('not.be.checked')
+    })
+
+    it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function() {
+        cy.get('input[id="firstName"]').type('Sabrynna')
+        cy.get('input[id="lastName"]').type('Lourenço')
+        cy.get('input[type="email"]').type('limasabrynna@gmail.com')
+        cy.get('#phone-checkbox').check()
+        cy.get('textarea[id="open-text-area"]').type('Cadastro de usuário exatamente digitado rápido')
+        cy.contains('button', 'Enviar').click()
+
+        cy.get('.error').should('be.visible')
     })
 })
 
